bd63c4a99d91291f4f36f0a898551e91e5ea7c61
Teninlanimi Taiwo <taiwoteninlanimi@gmail.com>
2024-12-04 15:31:42 +0100

    Fixed Members Issue

--- src/main/java/org/niit_project/backend/controller/ChatController.java
+++ src/main/java/org/niit_project/backend/controller/ChatController.java
@@ -1,6 +1,10 @@
 package org.niit_project.backend.controller;
 
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
+import io.github.cdimascio.dotenv.Dotenv;
 import org.niit_project.backend.dto.ApiResponse;
+import org.niit_project.backend.entities.AttachmentType;
 import org.niit_project.backend.entities.Chat;
 import org.niit_project.backend.entities.MessageType;
 import org.niit_project.backend.service.ChatService;
@@ -9,6 +13,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("scholarly/api/v1/chat")
@@ -54,6 +59,42 @@ public class ChatController {
         catch (Exception e){
             response.setMessage(e.getMessage());
             return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PostMapping(path = "/sendAttachment/{channelId}/{senderId}", consumes = "multipart/form-data")
+    public ResponseEntity<ApiResponse> sendChat(@PathVariable String channelId, @PathVariable String senderId, @RequestPart("attachment") MultipartFile attachment, @RequestPart("attachmentType")AttachmentType type, @RequestPart("message") String message){
+        var response = new ApiResponse();
+
+        try{
+            var dotenv = Dotenv.load();
+            var cloudinary = new Cloudinary(dotenv.get("CLOUDINARY_URL"));
+
+            var params = ObjectUtils.asMap(
+                    "use_filename", true,
+                    "unique_filename", false,
+                    "overwrite", true
+            );
+
+            var result = cloudinary.uploader().upload(attachment.getBytes(), params);
+            var secure_url = result.get("secure_url");
+
+            var chat = new Chat();
+            chat.setAttachment(secure_url.toString());
+            chat.setChannelId(channelId);
+            chat.setSenderId(senderId);
+            chat.setMessage(message);
+            chat.setMessageType(MessageType.chat);
+            chat.setAttachmentType(type == null? AttachmentType.image: type);
+
+            var createdChat = chatService.createChat(chat, channelId, senderId);
+            response.setMessage("Uploaded Attachment Successfully");
+            response.setData(createdChat);
+            return new ResponseEntity<>(response, HttpStatus.OK);
+
+        } catch (Exception e) {
+            response.setMessage(e.getMessage());
+            return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
         }
     }
 }
