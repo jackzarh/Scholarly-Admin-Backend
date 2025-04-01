9edc38c86024499686f7a98d8d0ed161961b476f
Teninlanimi Taiwo <taiwoteninlanimi@gmail.com>
2025-04-01 09:10:06 +0100

    Created MenteeService class

--- src/main/java/org/niit_project/backend/service/MenteeService.java
+++ src/main/java/org/niit_project/backend/service/MenteeService.java
@@ -1,9 +1,19 @@
 package org.niit_project.backend.service;
 
+import org.niit_project.backend.entities.Student;
+import org.niit_project.backend.enums.AdminRole;
+import org.niit_project.backend.models.ApiException;
+import org.niit_project.backend.models.Mentee;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.aggregation.Aggregation;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.http.HttpStatus;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class MenteeService {
@@ -14,8 +24,41 @@ public class MenteeService {
     @Autowired
     private SimpMessagingTemplate messagingTemplate;
 
+    @Autowired
+    private AdminService adminService;
 
-    public List<Mentee> getMentees(String counselorId){
+
+    public List<Mentee> getMentees(String counselorId) throws ApiException {
+        // Check if admin exists. Throw an error if they do not exist
+        var admin = adminService.getAdmin(counselorId).orElseThrow(() -> new ApiException("Admin doesn't exist", HttpStatus.NOT_FOUND));
+
+        // Make sure admin is a counselor
+        if(admin.getRole() != AdminRole.counselor){
+            throw new ApiException("This admin is not a counselor", HttpStatus.UNAUTHORIZED);
+        }
+
+        // Create Aggregation Operations
+        var match = Aggregation.match(Criteria.where("counselorId").is(counselorId));
+        var sort = Aggregation.sort(Sort.Direction.DESC, "createdAt");
+        var aggregation = Aggregation.newAggregation(match, sort);
+
+        var results = mongoTemplate.aggregate(aggregation, "students", Student.class).getMappedResults();
+
+        var mentees = results.stream().map(student -> {
+            var mentee = new Mentee();
+            mentee.setFirstName(student.getFirstName());
+            mentee.setLastName(student.getLastName());
+            mentee.setColor(student.getColor());
+            mentee.setCreatedTime(student.getCreatedAt());
+            mentee.setProfile(student.getProfile());
+
+            // Line containing functionalities to set the status
+
+            return mentee;
+        }).toList();
+
+        return mentees;
+
 
     }
 }
